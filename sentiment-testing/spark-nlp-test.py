36609ff17bc5d5dc986c6eb41a399c9be2ba80d6
Elijah Jasso <elijahjasso19@gmail.com>
2020-11-18 18:44:40 -0800

    testing spark nlp

---
+++ sentiment-testing/spark-nlp-test.py
@@ -0,0 +1,21 @@
+from sparknlp.base import *
+from sparknlp.annotator import *
+from sparknlp.pretrained import PretrainedPipeline
+import sparknlp
+
+spark = sparknlp.start(gpu=True)
+
+# Download a pre-trained pipeline
+pipeline = PretrainedPipeline('explain_document_dl', lang='en')
+
+# Your testing dataset
+text = """
+The Mona Lisa is a 16th century oil painting created by Leonardo.
+It's held at the Louvre in Paris.
+"""
+
+# Annotate your testing dataset
+result = pipeline.annotate(text)
+
+# What's in the pipeline
+list(result.keys())
